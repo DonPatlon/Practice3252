fd786e33778fa7d0e36989d478edaaade1732dc4
DonPatlon <racistcommunist@gmail.com>
2021-07-09 01:20:16 +0300

    инциализация проекта (то что сделано без сохранения в JSON)

---
+++ main.py
@@ -0,0 +1,126 @@
+from tkinter import *
+from PIL import ImageTk, Image
+import tkinter as tk
+from tkinter import filedialog
+import random
+
+WIDTH, HEIGHT = 900, 900
+topx, topy, botx, boty = 0, 0, 0, 0
+rect_id = None
+n=0
+window = tk.Tk()
+window.title("ratatata")
+window.geometry('%sx%s' % (WIDTH, HEIGHT))
+window.configure(background='grey')
+a = {}
+k=[]
+
+class drawrect(object):
+    def __init__(self,canvas,topx, topy, botx, boty,color):
+        global s
+        s = textExample.get("1.0", "end")
+        self.canvas = canvas
+        self.id=canvas.create_rectangle(topx, topy, botx, boty, fill='', outline=color, tags=(s,'rect'),width=5)
+
+
+
+def open_img():
+    global canvas
+    global rect_id
+    path = filedialog.askopenfilename(title='open')
+    img = Image.open(path)
+
+    img = ImageTk.PhotoImage(img)
+    canvas = tk.Canvas(window, width=img.width(), height=img.height(),
+                       borderwidth=0, highlightthickness=0)
+    canvas.pack(expand=True)
+    canvas.img = img
+    canvas.create_image(0, 0, image=img, anchor=tk.NW)
+    rect_id = canvas.create_rectangle(topx, topy, topx, topy,
+                                      dash=(2, 2), fill='', outline='white')
+
+    canvas.bind('<Button-1>', get_mouse_posn)
+    canvas.bind('<B1-Motion>', update_sel_rect)
+
+
+def get_mouse_posn(event):
+    global topy, topx,item
+    z=0
+    topx, topy = event.x, event.y
+    item = canvas.find_closest(event.x, event.y)[0]
+    #print(item)
+    tags = canvas.gettags(item)
+    #print(tags)
+
+
+def update_sel_rect(event):
+
+    global rect_id
+    global topy, topx, botx, boty
+
+    botx, boty = event.x, event.y
+    canvas.coords(rect_id, topx, topy, botx, boty)
+
+
+def drrect():
+    global canvas,a,k
+    color1 = 'white'
+    obj = drawrect(canvas,  topx, topy, botx, boty, color1)
+    a[obj.id] = obj
+    print(topx, topy, botx, boty)
+
+
+def new_tag():
+    global item
+    if item==1 or item==2:
+        print(1)
+    else:
+        canvas.addtag_withtag(textExample.get("1.0", "end"), item)
+def delete_tag():
+    global item
+    if item == 1 or item == 2:
+        print(1)
+    else:
+        canvas.dtag(item, textExample.get("1.0", "end"))
+def change_colors():
+    colors = ["red", "orange", "yellow", "green", "blue", "violet"]
+    canvas.itemconfigure(textExample.get("1.0", "end"), outline = random.choice(colors))
+
+
+def change_colors1():
+    canvas.itemconfigure(textExample.get("1.0", "end"), outline = 'white')
+
+def delete_rectangle():
+    global item
+    if item==1 or item==2:
+        print(1)
+    else:
+        canvas.delete(item)
+
+def delete_img():
+    canvas.destroy()
+    list.clear(a)
+def lll():
+    global a
+    for i in a:
+        tags = canvas.gettags(i)
+        print(tags)
+   # print(tags)
+
+
+
+textExample=tk.Text(window, width=15, height=1)
+textExample.place(x=145, y=50)
+btn = Button(window, text='Открыть изображение', command=open_img).place(x=1, y=1)
+btn1 = Button(window, text='Выделить область ', command=drrect).place(x=140, y=1)
+btn2 = Button(window, text='Назначить выделение ', command=change_colors).place(x=6, y=50)
+btn3 = Button(window, text='Снять выделение ', command=change_colors1).place(x=275, y=50)
+btn4 = Button(window, text='Удалить картинку ', command=delete_img).place(x=1024, y=0)
+btn5 = Button(window, text='Удалить квадрат ', command=delete_rectangle).pack()
+btn6 = Button(window, text='Добавить тег', command=new_tag).pack()
+btn7 = Button(window, text='Удалить тег', command=delete_tag).pack()
+btn8 = Button(window, text='У тег', command=lll).pack()
+btn9 = Button(window, text='Super slaves', command=lll).pack()
+
+
+window.mainloop()
